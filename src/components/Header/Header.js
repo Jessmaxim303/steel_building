5bb7c958276ef973069c219f5684f329e2cec867
Jesse Maxim <jessmaxim303@gmail.com>
2020-05-29 09:30:12 -0600

    Missing home page assets

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Header.css';
+import logo from '../Assets/logo.png';
 
 export const Header = () => {
 	return (
 		<section className="header_container">
+		  <img className="header_logo" src={logo}/>
 		  <div className="header_menu_container">
+		    <h1>1(800)497-2135</h1>
 			  <h1>HOME</h1>
 			  <h1>STEEL BUILDINGS</h1>
 			  <h1>PROJECTS</h1>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import './Home.css';
 import { Header } from '../Header/Header.js';
-import redsteel from '../Assets/barn.png';
+import { Welcome } from '../Welcome/Welcome.js';
+import { Products } from '../Products/Products.js';
+import { Services } from '../Services/Services.js';
 
 export const Home = () => {
 	return (
 		<section>
 		  <Header/>
-		  <section className="home_main_container">
-		  	<div className="home_intro_welcome inner-border">
-		  	  <div className="home_info_container">
-		  	    <h1 className="home_intro_text">PREMIUM PRE-ENGINEERED <br/> STEEL BUILDINGS</h1>
-		  		  <button className="home_quote_button">Request a Quote</button>
-		  		  <button className="home_product_button">View Out Steel Buildings</button>
-		  		</div>  
-		  		<img className="home_redsteel_img" src={redsteel}/>
-		  	</div>
-		    <section className="home_alert">
-		      <h2>
-		        COVID-19 UPDATE 
-		      </h2>
-		      <h3>WE’RE HERE FOR OUR CUSTOMERS AND EMPLOYEES. <br/> SEE THIS MESSAGE FROM OUR FOUNDERS FOR <br/> MORE INFORMATION.
-          </h3>
-		    </section>
-		  </section>
+		  <Welcome/>
+		  <Products/>
+		  <Services/>
 		</section>  
 		)
 }
