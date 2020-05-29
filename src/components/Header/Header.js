535cffc871323efee6ec367464b117ace406add6
Jesse Maxim <jessmaxim303@gmail.com>
2020-05-28 21:31:26 -0600

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+	return (
+		<section className="header_container">
+		  <div className="header_menu_container">
+			  <h1>HOME</h1>
+			  <h1>STEEL BUILDINGS</h1>
+			  <h1>PROJECTS</h1>
+			  <h1>REGIONAL</h1>
+			  <h1>ABOUT</h1>
+			  <h1>CONTACT</h1>
+			</div>  
+		</section>
+		)
+}
